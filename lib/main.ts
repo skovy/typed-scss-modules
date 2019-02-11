731f0dc434c94f7454ca53e7c417430169f5258a
Spencer Miskoviak <smiskoviak@gmail.com>
2019-02-10 18:18:35 -0800

    feat(directories): support directories as well as globs

--- lib/main.ts
+++ lib/main.ts
@@ -8,19 +8,39 @@ import { Options, fileToClassNames } from "./sass";
 import { classNamesToTypeDefinitions } from "./typescript";
 
 const error = (message: string) => console.log(chalk.red(`[ERROR] ${message}`));
+const warn = (message: string) => console.log(chalk.yellowBright(`${message}`));
 const notice = (message: string) => console.log(chalk.gray(`${message}`));
 const success = (message: string) => console.log(chalk.green(message));
 
 export const main = (pattern: string, options: Options): void => {
-  const files = glob.sync(pattern);
+  // When the provided pattern is a directory construct the proper glob to find
+  // all .scss files within that directory. Also, add the directory to the
+  // included paths so any imported with a path relative to the root of the
+  // project still works as expected without adding many include paths.
+  if (fs.lstatSync(pattern).isDirectory()) {
+    if (Array.isArray(options.includePaths)) {
+      options.includePaths.push(pattern);
+    } else {
+      options.includePaths = [pattern];
+    }
+
+    // When the pattern provide is a directory, assume all .scss files within.
+    pattern = path.resolve(pattern, "**/*.scss");
+  }
+
+  // Find all the files that match the provied pattern. Always ignore
+  // node_modules anywhere in the directory tree.
+  const files = glob.sync(pattern, { ignore: "**/node_modules/**" });
 
   if (!files || !files.length) {
     error("No files found.");
     return;
   }
 
+  // This case still works as expected but it's easy to do on accident so
+  // provide a (hopefully) helpful warning.
   if (files.length === 1) {
-    notice(
+    warn(
       'Only 1 file found. If using a glob pattern (eg: dir/**/*.scss) make sure to wrap in quotes (eg: "dir/**/*.scss").'
     );
   }
