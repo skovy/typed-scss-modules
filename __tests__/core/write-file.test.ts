d2eb412f1f8d5dd690fde881d845037bb8083c69
Spencer Miskoviak <smiskoviak@gmail.com>
2019-02-16 15:58:08 -0800

    test(core): add more tests for core functionality

---
+++ __tests__/core/write-file.test.ts
@@ -0,0 +1,46 @@
+import fs from "fs";
+
+import { writeFile } from "../../lib/core";
+import { getTypeDefinitionPath } from "../../lib/typescript";
+
+describe("writeFile", () => {
+  beforeEach(() => {
+    // Only mock the write, so the example files can still be read.
+    fs.writeFileSync = jest.fn();
+    console.log = jest.fn();
+  });
+
+  test("writes the corresponding type definitions for a file and logs", async () => {
+    const testFile = `${__dirname}/../style.scss`;
+    const typesFile = getTypeDefinitionPath(testFile);
+
+    await writeFile(testFile, {
+      watch: false,
+      exportType: "named"
+    });
+
+    expect(fs.writeFileSync).toBeCalledWith(
+      typesFile,
+      "export const someClass: string;\n"
+    );
+
+    expect(console.log).toBeCalledWith(
+      expect.stringContaining(`[GENERATED TYPES] ${typesFile}`)
+    );
+  });
+
+  test("it skips files with no classes", async () => {
+    const testFile = `${__dirname}/../empty.scss`;
+
+    await writeFile(testFile, {
+      watch: false,
+      exportType: "named"
+    });
+
+    expect(fs.writeFileSync).not.toBeCalled();
+
+    expect(console.log).toBeCalledWith(
+      expect.stringContaining(`[NO GENERATED TYPES] ${testFile}`)
+    );
+  });
+});
--- __tests__/sass/file-to-class-names.test.ts
+++ __tests__/sass/file-to-class-names.test.ts
@@ -2,14 +2,14 @@ import { fileToClassNames } from "../../lib/sass";
 
 describe("fileToClassNames", () => {
   test("it converts a file path to an array of class names (default camel cased)", async () => {
-    const result = await fileToClassNames(`${__dirname}/style.scss`);
+    const result = await fileToClassNames(`${__dirname}/../complex.scss`);
 
     expect(result).toEqual(["someStyles", "nestedClass", "nestedAnother"]);
   });
 
   describe("nameFormat", () => {
     test("it converts a file path to an array of class names with kebab as the name format", async () => {
-      const result = await fileToClassNames(`${__dirname}/style.scss`, {
+      const result = await fileToClassNames(`${__dirname}/../complex.scss`, {
         nameFormat: "kebab"
       });
 
@@ -17,7 +17,7 @@ describe("fileToClassNames", () => {
     });
 
     test("it converts a file path to an array of class names with param as the name format", async () => {
-      const result = await fileToClassNames(`${__dirname}/style.scss`, {
+      const result = await fileToClassNames(`${__dirname}/../complex.scss`, {
         nameFormat: "param"
       });
 
