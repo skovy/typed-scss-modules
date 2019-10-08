4565e7af8d61d10b0e3692e601c91dbad77a9f9a
peanutbother <jg@jabuco-it.de>
2019-10-08 13:55:18 +0200

    update deps; fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)"],
   testPathIgnorePatterns: [
     "<rootDir>/dist/",
     "<rootDir>/node_modules/",
     "(.*).d.ts"
-  ]
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  modulePathIgnorePatterns: ["dist"]
 };
