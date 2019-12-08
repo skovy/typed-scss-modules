d456d21bcececb3a5b94e094e04f639206699315
Spencer Miskoviak <smiskoviak@gmail.com>
2019-12-08 10:48:19 -0800

    test(ignore): add failing test case

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -17,7 +17,8 @@ describe("main", () => {
       watch: false,
       ignoreInitial: false,
       exportType: "named",
-      listDifferent: false
+      listDifferent: false,
+      ignore: null
     });
 
     const expectedDirname = slash(__dirname);
@@ -31,6 +32,36 @@ describe("main", () => {
     expect(fs.writeFileSync).toBeCalledWith(
       `${expectedDirname}/style.scss.d.ts`,
       "export const someClass: string;\n"
+    );
+  });
+
+  test("generates types for all .scss files and ignores files that match the ignore pattern", async () => {
+    const pattern = `${__dirname}`;
+
+    await main(pattern, {
+      watch: false,
+      ignoreInitial: false,
+      exportType: "named",
+      listDifferent: false,
+      ignore: "**/style.scss"
+    });
+
+    expect(fs.writeFileSync).toBeCalledTimes(3);
+
+    const expectedDirname = slash(__dirname);
+    expect(fs.writeFileSync).toBeCalledWith(
+      `${expectedDirname}/complex.scss.d.ts`,
+      "export const someStyles: string;\nexport const nestedClass: string;\nexport const nestedAnother: string;\n"
+    );
+
+    // Files that should match the ignore pattern.
+    expect(fs.writeFileSync).not.toBeCalledWith(
+      `${expectedDirname}/style.scss.d.ts`,
+      expect.anything()
+    );
+    expect(fs.writeFileSync).not.toBeCalledWith(
+      `${expectedDirname}/nested-styles/style.scss.d.ts`,
+      expect.anything()
     );
   });
 });
