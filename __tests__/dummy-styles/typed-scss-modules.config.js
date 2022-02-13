0758395393bfe48ffce47ffa4c5f10eef9ac192c
Spencer Miskoviak <smiskoviak@gmail.com>
2022-02-13 16:13:43 -0700

    chore: add main test coverage

---
+++ __tests__/dummy-styles/typed-scss-modules.config.js
@@ -0,0 +1,14 @@
+export const config = {
+  banner: "",
+  watch: false,
+  ignoreInitial: false,
+  exportType: "named",
+  exportTypeName: "ClassNames",
+  exportTypeInterface: "Styles",
+  listDifferent: false,
+  ignore: [],
+  implementation,
+  quoteType: "single",
+  updateStaleOnly: false,
+  logLevel: "verbose",
+};
