19c0043af876aba90731ee3b3ba8dbe593c25d2c
Spencer Miskoviak <smiskoviak@gmail.com>
2022-02-16 19:56:48 -0700

    test: update get type def path helper tests

--- __tests__/typescript/get-type-definition-path.test.ts
+++ __tests__/typescript/get-type-definition-path.test.ts
@@ -1,13 +1,30 @@
-import { ConfigOptions } from "../../lib/core";
+import path from "path";
+
+import { DEFAULT_OPTIONS } from "../../lib/load";
 import { getTypeDefinitionPath } from "../../lib/typescript";
 
 describe("getTypeDefinitionPath", () => {
-  it("returns the type definition path", () => {
-    const path = getTypeDefinitionPath(
-      "/some/path/style.scss",
-      {} as ConfigOptions
-    );
+  const cssFilePath = path.resolve(process.cwd(), "some/path/style.scss");
 
-    expect(path).toEqual("/some/path/style.scss.d.ts");
+  it("returns the type definition path", () => {
+    const outputPath = getTypeDefinitionPath(cssFilePath, DEFAULT_OPTIONS);
+
+    expect(outputPath).toEqual(`${cssFilePath}.d.ts`);
+  });
+
+  describe("when outputFolder is passed", () => {
+    it("returns the type definition path", () => {
+      const outputPath = getTypeDefinitionPath(cssFilePath, {
+        ...DEFAULT_OPTIONS,
+        outputFolder: "__generated__",
+      });
+
+      const generatedFilePath = path.resolve(
+        process.cwd(),
+        "__generated__/some/path/style.scss.d.ts"
+      );
+
+      expect(outputPath).toEqual(generatedFilePath);
+    });
   });
 });
