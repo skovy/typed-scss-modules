8ce9540a6ee3c98663c8f031a0b97b22aaf92ba1
Spencer Miskoviak <smiskoviak@gmail.com>
2022-04-25 19:55:27 -0600

    fix: alias and aliasPrefixes in config file

--- examples/config-file/typed-scss-modules.config.js
+++ examples/config-file/typed-scss-modules.config.js
@@ -1,6 +1,8 @@
 const jsonImporter = require("node-sass-json-importer");
 
 export const config = {
+  aliases: { "not-real": "test-value" },
+  aliasPrefixes: { "also-not-real": "test-value" },
   banner: "// config file banner",
   nameFormat: "kebab",
   exportType: "default",
