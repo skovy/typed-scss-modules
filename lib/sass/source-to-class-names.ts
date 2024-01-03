dd9a79443b44f1cf57eea3118dc1a1a27039e281
George Taveras <gtaveras@theknotww.com>
2024-01-03 13:42:10 -0500

    fix(cli): loading of downstream CSS modules
    Use an identity-obj-proxy to avoid loading downstream files. This solves
    the problem called out here[^1], while minimizing disk access, at the
    cost of issues with broken imports.
    
    [^1]: https://github.com/skovy/typed-scss-modules/pull/218#issuecomment-1875677110

--- lib/sass/source-to-class-names.ts
+++ lib/sass/source-to-class-names.ts
@@ -1,8 +1,28 @@
+import * as proxy from "identity-obj-proxy";
 import postcss from "postcss";
 import PostcssModulesPlugin from "postcss-modules";
 import { ConfigOptions } from "../core/types";
 
 export type SourceToClassNamesOptions = Pick<ConfigOptions, "modules">;
+
+/**
+ * Use identity-obj-proxy for any imported CSS modules
+ *
+ * This should not affect the final for a given file, since the types are
+ * generated only using the classes defined in the current file.
+ *
+ * This has the added benefit of reducing system calls and disk access,
+ * so may perform faster in large projects.
+ *
+ * The only drawback is that this does not process the import tree
+ * the way in which it is intended to be used, so broken imports
+ * will not be surfaced by this tool.
+ */
+class IdentityObjProxyLoader {
+  async fetch() {
+    return proxy;
+  }
+}
 
 /**
  * Converts a CSS source string to a list of exports (class names, keyframes, etc.)
@@ -16,6 +36,7 @@ export const sourceToClassNames = async (
   await postcss([
     PostcssModulesPlugin({
       ...modules,
+      Loader: IdentityObjProxyLoader,
       getJSON: (_, json) => {
         result = json;
       },
