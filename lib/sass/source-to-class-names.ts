edd6f7f3495c4d5f974935003cf4ceda9ed7dd86
George Taveras <gtaveras@theknotww.com>
2024-01-16 19:43:44 -0500

    fix(core): loading of downstream CSS modules

--- lib/sass/source-to-class-names.ts
+++ lib/sass/source-to-class-names.ts
@@ -1,5 +1,25 @@
+import * as proxy from "identity-obj-proxy";
 import postcss from "postcss";
 import PostcssModulesPlugin from "postcss-modules";
+
+/**
+ * Use identity-obj-proxy for any imported CSS modules
+ *
+ * This should not affect the final types for a given file, since the types are
+ * generated only using the classes defined in the current file and not its imports.
+ *
+ * This has the added benefit of reducing system calls and disk access,
+ * so may perform faster in large projects.
+ *
+ * The only drawback is that this does not process the import tree
+ * the way in which it is intended to be used, so broken imports
+ * will not be surfaced by this tool.
+ */
+class IdentityObjProxyLoader {
+  async fetch() {
+    return proxy;
+  }
+}
 
 /**
  * Converts a CSS source string to a list of exports (class names, keyframes, etc.)
@@ -11,6 +31,7 @@ export const sourceToClassNames = async (
   let result: Record<string, string> = {};
   await postcss([
     PostcssModulesPlugin({
+      Loader: IdentityObjProxyLoader,
       getJSON: (_, json) => {
         result = json;
       },
